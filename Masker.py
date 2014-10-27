9ad6f59238a0b5a0fabaada543b8bebc49713ee8
DamTerrion <damterrion@yandex.ru>
2014-10-27 19:36:14 +0300

    Second victory. It's alive

--- Masker.py
+++ Masker.py
@@ -2,21 +2,20 @@
 
 from PIL import Image
 from copy import deepcopy
+from time import ctime, time as now
 
-def mask (img, condition=255):
+def mask (img, condition=None):
     matrix = []
     pixels = 0
     approved = 0
     pix = img.load()
-    if isinstance(condition, int) and condition < 256:
-        condition = (condition,
-                     condition,
-                     condition)
     for j in range(img.size[1]):
         matrix.append([])
         for i in range(img.size[0]):
             pixels += 1
-            if pix[i,j] == condition:
+            if (
+                (condition and pix[i,j] == condition) or
+                (not condition and pix[i,j])):
                 matrix[j].append(1)
                 approved += 1
             else:
@@ -85,7 +84,7 @@ def count (matrix, mode='shallow'):
 
 def purge (matrix):
     old = matrix
-    print count(old), 'elements in progress.'
+    # print count(old), 'elements in progress.'
     new = deepcopy(matrix)
     for j in range(len(new)):
         for i in range(len(new[j])):
@@ -106,22 +105,20 @@ def purge (matrix):
                 old[n+j][m+i] = 0
     return new
 
-def printf (matrix):
-    for string in matrix:
-        print string
-    print ''
-
 def make_dxf (matrix, px_size=1, center=(0, 0), layer='RASTER'):
     if isinstance(matrix, str):
-        if matrix = 'header':
+        if matrix == 'header':
             return ['  0', 'SECTION',
                     '  2', 'ENTITIES']
+        if matrix == 'end':
+            return ['  0', 'ENDSEC',
+                    '  0', 'EOF']
     counter = 0
-    stack = 
+    stack = []
     size = (px_size*len(matrix[0]),
             px_size*len(matrix))
     start = (center[0] - size[0]/2,
-             center[0] + size[1]/2)
+             center[1] + size[1]/2)
     stack.extend(['  0', 'POLYLINE',
                   '  8', str(layer)+'_b',
                   ' 66', '     1',
@@ -178,54 +175,104 @@ def make_dxf (matrix, px_size=1, center=(0, 0), layer='RASTER'):
                     '  8', str(layer)+'_t'
                     ])
                 counter += 1
-    stack.extend(['  0', 'ENDSEC',
-                  '  0', 'EOF'])
     return stack, counter
 
 def split (img, size=(100, 100)):
     stack = []
+    box = []
+    border = (
+        -img.size[0]/2,
+        +img.size[1]/2
+        )
     if isinstance(size, int):
         size = (size, size)
-    num = (img.size[0]//size[0]+1,
-           img.size[1]//size[1]+1)
-    residue = (img.size[0]%size[0],
-               img.size[1]%size[1])
-    box = size
+    num = [
+        img.size[0]//size[0]+1,
+        img.size[1]//size[1]+1
+        ]
+    residue = [
+        img.size[0]%size[0],
+        img.size[1]%size[1]
+        ]
+    if residue[0] == 0:
+        num[0] -= 1
+        residue[0] = size[0]
+    if residue[1] == 0:
+        num[1] -= 1
+        residue[1] = size[1]
+    box.extend(size)
     if num == (1, 1):
         return [(0, 0, img)]
     for j in range(num[1]):
-        box[0] == size[0]
+        box[0] = size[0]
         if j == num[1]-1:
             box[1] = residue[1]
         for i in range(num[0]):
             if i == num[0]-1:
                 box[0] = residue[0]
-            stack.append((i, j, img.crop((i*size[0], j*size[1],
-                                          i*size[0]+box[0],
-                                          i*size[1]+box[1]))
+            x = (border[0]+i*size[0]+box[0]/2,
+                 i*size[0],
+                 i*size[0]+box[0])
+            y = (border[1]-j*size[1]-box[1]/2,
+                 j*size[1],
+                 j*size[1]+box[1])
+            stack.append((x[0], y[0], img.crop((x[1], y[1], x[2], y[2]))
                           ))
     return stack
 
-def do (filename=None):
+def test_img (img_name=False):
+    if not img_name: img_name = input('Filename: ')
+    img = Image.open(img_name+'.png')
+    hist = dict()
+    for j in range(img.size[1]):
+        for i in range(img.size[0]):
+            pixel = img.getpixel((i, j))
+            px_name = str(pixel)
+            if px_name in hist:
+                hist[px_name] += 1
+            else:
+                hist[px_name] = 1
+    for item in hist:
+        print item, hist[item]
+
+def do (filename=None,
+        px_status=None,
+        img_center=(0, 0),
+        px_size=1,
+        fragm_size=None
+        ):
+    start_time = now()
+    lines_counter = 0
+    dxf_body = []
     if not filename: filename = input('Filename: ')
     img = Image.open(filename+'.png')
     print ' '.join(('File', filename+'.png', 'loaded with',
                     str(img.size[0])+'x'+str(img.size[1]), 'px.'))
-    splitted = split(img)
-    print 'Image was divided for', len(splitted), 'fragments.'
-    full = []
-    for rec in splitted:
-        prepared, px_counter = mask(rec[2])
-        print px_counter[1], 'from', px_counter[0], 'px founded in fragment.'
-        print count(prepared), 'px prepared to processing.'
+    if fragm_size:
+        splitted = split(img, fragm_size)
+        print 'Image was divided for', len(splitted), 'fragments.'
+        fragm_num = 0
+        for fragment in splitted:
+            fragm_num += 1
+            prepared, px_counter = mask(fragment[2], px_status)
+            if px_counter[1] != 0:
+                print px_counter[1], 'from', px_counter[0], 'px founded in fragment ¹', fragm_num
+            result = purge(prepared)
+            dxf_fragm, addition = make_dxf(
+                result, px_size,
+                (fragment[0]*px_size + img_center[0],
+                 fragment[1]*px_size + img_center[1]),
+                filename)
+            dxf_body.extend(dxf_fragm)
+            lines_counter += addition
+    else:
+        repared, px_counter = mask(img, px_status)
+        print px_counter[1], 'from', px_counter[0], 'px founded in image.'
         result = purge(prepared)
-        print count(result, ('deep', 'mult')), 'elements processed.'
-        full.append((rec[0], rec[1], result))
-        dxf_fragm, addition = make_dxf(result, 0.02,
-                             (rec[0]*200, rec[1]*200),
-                             filename)
+        dxf_body, lines_counter = make_dxf(
+            result, px_size, img_center, filename)
+    
     dxf_head = make_dxf('header')
-    dxf_body, lines_counter = make_dxf(result, 0.02, (63.5, 63.5), filename)
     dxf_end = make_dxf('end')
     dxf_text = '\n'.join(dxf_head +
                          dxf_body +
@@ -234,7 +281,24 @@ def do (filename=None):
     output = open(filename+'.dxf', 'w')
     output.write(dxf_text)
     output.close()
-    print 'File saved.'
+    end_time = now()
+    result_time = end_time - start_time
+    if result_time >= 60:
+        mins = result_time//60
+        secs = result_time%60
+        result_time = str(mins)+' m. '+str(secs)+' sec.'
+    else: result_time = str(result_time)+' sec.'
+    print 'File saved in', result_time
+    log = open('masker.log', 'a')
+    log.write(''.join((ctime(end_time), '\t',
+                       filename, '\t',
+                       'in ', result_time, '\n'
+                       ))
+              )
+    log.close()
 
 if __name__ == '__main__':
-    do()
+    do('Craft1', None, (63.5, 63.5), 0.02, (100, 100))
+    do('Craft2', None, (63.5, 63.5), 0.02, (100, 100))
+    do('Craft3', None, (63.5, 63.5), 0.02, (100, 100))
+    pass
